148dea91778fe056d92420294387451771b1402c
Manthan Mallikarjun <nahtnam@gmail.com>
2018-09-29 23:24:59 -0700

    make multi-story the home page

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+// import Home from './views/Home.vue';
+import MultiStory from './views/MultiStory.vue';
 
 Vue.use(Router);
 
@@ -8,10 +9,19 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: Home,
+    // },
     {
       path: '/',
-      name: 'home',
-      component: Home,
+      name: 'MultiStory',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      // component: () => import(/* webpackChunkName: "about" */ './views/MultiStory.vue'),
+      component: MultiStory,
     },
     {
       path: '/multi-story',
