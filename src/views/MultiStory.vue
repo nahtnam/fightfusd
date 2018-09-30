54c2481fba16c85b7aca8c381b907d28c7d6f467
Manthan Mallikarjun <nahtnam@gmail.com>
2018-09-29 23:17:00 -0700

    update multi-story page

--- src/views/MultiStory.vue
+++ src/views/MultiStory.vue
@@ -4,40 +4,35 @@
       <div class="hero-body">
         <div class="container">
           <h1 class="title">
-            Starting tomorrow (September 11th, 2018), No More 2nd Story Construction?
+            Second Floor Construction to be Limited to Max 1000 sq. ft. in R1-6 Zone
           </h1>
           <h2 class="subtitle">
-            That's right. The Fremont city council is proposing to stop 2nd story construction permits completely.
+            That's right. The Fremont city council is proposing to limit 2nd story construction permits.
           </h2>
         </div>
       </div>
     </section>
     <section class="section is-paddingless">
       <div class="container content">
-        <h4>
-          The Fremont City Council has come up with 2 alternatives given below:
-        </h4>
-        <ul>
-          <li>
-            Alt 3: Restricts the second Floor to be only 30% of the first floor and allows the first floor to be 45% of the lot
-          </li>
-          <li>
-            Alt 7-2: Even stricter. Total allowed (second floor + first floor) 45% of the lot AND second floor cannot exceed 30% of the first floor.
-          </li>
-        </ul>
-        <strong class="is-uppercase">If the board does not agree on one of these proposals, then all 2nd story permits will be stopped!</strong>
-        <h3>Both proposals are not acceptable and completely stopping 2nd story construction permits is not acceptable either.</h3>
-        <h2><small>We need your help to stop this:</small>&nbsp;<u>Please do the following ASAP!!!</u></h2>
+        <h3>
+          Tuesday, October 2nd Fremont city council board is poised to approve second-floor construction to max 1000 sq. ft. in the residential R1-6 Zone. There are also restrictions imposed on other residential zoning districts.
+        </h3>
+        <div class="has-text-centered">
+          <a href="http://fremontcityca.iqm2.com/Citizens/FileOpen.aspx?Type=1&ID=1479&Inline=True" target="_blank" class="button is-success is-medium">
+            For more details visit the following web page
+          </a>
+        </div>
+        <h2><small>This is too restrictive. We need your help to stop this:</small>&nbsp;<u>Please do the following ASAP!!!</u></h2>
         <ol>
           <li>Send an Email <strong>TODAY (NOT TOMORROW)</strong> Sample email template below.</li>
-          <li>Attend the City Council Meeting on <strong>September 11, 2018 at 7:00 PM</strong></li>
+          <li>Attend the City Council Meeting on <strong>October 2, 2018 at 7:00 PM</strong></li>
           <li>If you would like to know more about the issue, please read the <a href="http://fremontcityca.iqm2.com/Citizens/Detail_LegiFile.aspx?Frame=&MeetingID=1620&MediaPosition=&ID=3549&CssClass=" target="_blank">full proposal</a>.</li>
         </ol>
         <hr>
         <div class="columns">
           <div class="column is-3">
             <h3 class="title is-marginless" style="padding-bottom: 5px;">City Council Meeting</h3>
-            Date: <strong>September, 11th 2018 (Tuesday)</strong><br>
+            Date: <strong>October, 2nd 2018 (Tuesday)</strong><br>
             Time: <strong>7:00 PM</strong><br>
             Location:<br>
             <strong>
@@ -45,7 +40,7 @@
               Fremont, CA 94538<br><br>
             </strong>
 
-            <a class="button is-fullwidth is-success" href="http://fremontcityca.iqm2.com/Citizens/Detail_Meeting.aspx?ID=1620" target="_blank">
+            <a class="button is-fullwidth is-success" href="http://fremontcityca.iqm2.com/Citizens/Detail_Meeting.aspx?ID=1622" target="_blank">
               Agenda
             </a>
             <br>
@@ -82,7 +77,7 @@
               I am a long-term resident of Fremont, and I see that the board is planning on restricting second story construction. Every new residential complex is multi-story because most people prefer having more than one floor. If people preferred single story homes, the builders would not be able to sell these multi-story ones. City council members who are elected by the people are trying to go against the needs of the majority of the people by favoring a few residents who are trying to bring up this restriction.
               <br>
               <br>
-              I along with my family strongly disagree on placing a moratorium on the construction of two-story homes and second-story additions just because city council members are not able to come up with an agreeable proposal. I also strongly oppose both of the proposed alternative (Alt 3 and Alt 7-2) of restricting 2nd story constructions. These proposals are too restrictive and not acceptable to the needs of most residents.
+              I along with my family strongly disagree on limiting the second floor to 1000 sq. ft. in R1-6 zone. It should be allowed at least up to 1500 sq.ft. The proposals approved by the board on September 11, 2018, are too restrictive and not acceptable to the needs of most residents.
               <br>
               <br>
               The city council must put this on the upcoming election ballot so that the residents of Fremont can decide on their own rather than a select group of individuals.
