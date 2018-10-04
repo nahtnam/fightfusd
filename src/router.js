9b3fc0363381b67447dc1f2eb3cd7fa3076184dd
Manthan Mallikarjun <nahtnam@gmail.com>
2018-10-03 19:41:39 -0700

    remove redirect and add old home page back

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-// import Home from './views/Home.vue';
+import Home from './views/Home.vue';
 
 Vue.use(Router);
 
@@ -8,14 +8,10 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: Home,
-    // },
     {
       path: '/',
-      redirect: '/multi-story',
+      name: 'home',
+      component: Home,
     },
     {
       path: '/multi-story',
